748bdb2ae482358959f0246768b394b12e60dbc6
josuemal98 <jmaldonadofuenntes@byupathway.edu>
2026-05-15 20:34:55 -0600

    js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,28 @@
+// select elements from the DOM
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('_____________');
+const list = document.querySelector('____');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
-li.textContent = input.value;
-deleteButton.textContent = 'Delete';
-li.append(deleteButton);
-list.append(li);
+button.addEventListener('click', function () {
+
+	if (input.value != '') {
+	
+		const li = document.createElement('li');
+		li.textContent = input.value;
+		
+		const deleteButton = document.createElement('button');
+		deleteButton.textContent = '❌';
+		deleteButton.addEventListener('click', function () {
+			list.removeChild(li);
+			input.focus();
+		});
+		
+		li.appendChild(deleteButton);
+
+		list.appendChild(li);
+	
+		input.value = '';
+	}
+
+	input.focus();
+});
